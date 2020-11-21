d78c1b86290e7159cec5f32677173b12aa2e68ce
megurt <mega.arte.99@gmail.com>
2020-11-21 11:54:09 +0300

    roles2

--- program.py
+++ program.py
@@ -8,26 +8,27 @@ class MyClient(discord.Client):
     async def on_ready(self):
         print('Logged on as', self.user)
 
-    async def on_raw_reaction_add(self, payload):
-    	channel = self.get_channel(payload.channel_id)
-    	message = await channel.fetch_message(payload.message_id)
-    	member = utils.get(message.guild.members, id=payload.user_id)
-
-
-    try:
-    	emoli = str(payload.emoji)
-    	role = utils.get(message.guild.roles, id= config.ROLES[emoji])
-
-    	if(len([i for i in member.roles if i.id not in config.EXCROLES]) <= config.MAX_ROLES_PER_USER):
-    		await member.add_roles(role)
-    		print('[SUCCESS] User {0.display_name} has been granted with role {1.name}'.format(member, role))
-    	else:
-    		await message.remove_reaction(payload.emoji, member)
-    		print('[ERROR] User {0.display_name} Too many roles for member {1.name}'.format(member, role))
-
-
-    except KeyError as e:
-    	print('[ERROR] KeyError, no role found for ' + emoji)
+     async def on_raw_reaction_add(self, payload):
+        if payload.message_id == config.POST_ID:
+            channel = self.get_channel(payload.channel_id) 
+            message = await channel.fetch_message(payload.message_id) 
+            member = utils.get(message.guild.members, id=payload.user_id) 
+ 
+        try:
+            emoji = str(payload.emoji) 
+            role = utils.get(message.guild.roles, id=config.ROLES[emoji]) 
+            
+            if(len([i for i in member.roles if i.id not in config.EXCROLES]) <= config.MAX_ROLES_PER_USER):
+                await member.add_roles(role)
+                print('[SUCCESS] User {0.display_name} has been granted with role {1.name}'.format(member, role))
+            else:
+                await message.remove_reaction(payload.emoji, member)
+                print('[ERROR] Too many roles for user {0.display_name}'.format(member))
+            
+        except KeyError as e:
+            print('[ERROR] KeyError, no role found for ' + emoji)
+        except Exception as e:
+            print(repr(e))
     	
 
     async def on_raw_reaction_remove(self, payload):
@@ -36,7 +37,7 @@ class MyClient(discord.Client):
         member = utils.get(message.guild.members, id=payload.user_id) 
  
         try:
-            emoji = str(payload.emoji)
+            emoji = str(payload.emoji) 
             role = utils.get(message.guild.roles, id=config.ROLES[emoji]) 
  
             await member.remove_roles(role)
