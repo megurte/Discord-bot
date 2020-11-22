63697fa003339bebee4cb2f199becd466e85e2c1
megurt <mega.arte.99@gmail.com>
2020-11-22 15:52:04 +0300

    Fixed commands Music.py

--- music.py
+++ music.py
@@ -8,48 +8,51 @@ from random import choice
 
 client = commands.Bot(command_prefix='!')
 
+help_text = {
+	'!hello - Поприветствовать бота \n'
+	'!ping - Пропинговать бота \n'
+	'!join - Подключить бота к голосовому каналу \n'
+	'!leave - Отключить бота из голосового канала \n'
+}
+
 
 @client.event
-async def on_ready(self):
-	print('Logged on as {0}!'.format(self.user)) 
+async def on_ready():
+	print('Logged on as Lulu-support#3215!') 
 
-@client.command()
+
+@client.command(name = join)
 async def join(ctx):
-	print("join")
-	global voice
-	channel = ctx.message.author.voice.channel
-	voice = get(client.voice_clients, guild = ctx.guild)
+	if not ctx.message.author.voice:
+		await ctx.send("You are not connected to a voice channel")
+		return
 
-	if voice and voice.is_connected():
-		await voice.move_to(channel)
 	else:
-		voice = await channel.connect()
-		await ctx.send('успешно подключился')
+		channel = ctx.message.author.voice.channel
+
+	await channel.connect()
 
 @client.command()
 async def leave(ctx):
-	print("leave")
-	global voice
-	channel = ctx.message.author.voice.channel
-	voice = get(client.voice_clients, guild = ctx.guild)
-
-	if voice and voice.is_connected():
-		await voice.disconnect()
-	else:
-		voice = await connect.channel()
-		await ctx.send('успешно отключился')
+	voice_client = ctx.message.guild.voice_client
+	await voice_client.disconnect()
 	
+@client.command()
+async def foo(ctx, arg1,arg2):
+	print("foo")
+	await ctx.send(arg1+arg2)
 
 @client.command()
-async def foo(ctx, arg):
-	print("foo")
-	await ctx.send(arg)
+async def help(ctx):
+	await 
+
 
 @client.command()
 async def hello(ctx):
 	responses = ['Привет, создатель!', 'Добро пожаловать', 'こんにちは～～', 'こんばんは、みんなさん', 'おかえりなさいご主人']
 	print("hello")
 	await message.channel.send(choice(responses))
+
 
 @client.command()
 async def ping(ctx):
