4e7a569b06f8a2c4e910e52bdb92d482bdeeecfe
megurt <mega.arte.99@gmail.com>
2020-11-22 16:08:05 +0300

    Help*

--- music.py
+++ music.py
@@ -8,12 +8,7 @@ from random import choice
 
 client = commands.Bot(command_prefix='!')
 
-help_text = {
-	'!hello - Поприветствовать бота \n'
-	'!ping - Пропинговать бота \n'
-	'!join - Подключить бота к голосовому каналу \n'
-	'!leave - Отключить бота из голосового канала \n'
-}
+status = ['Supporting!', 'Eating!', 'Sleeping!']
 
 
 @client.event
@@ -21,10 +16,23 @@ async def on_ready():
 	print('Logged on as Lulu-support#3215!') 
 
 
-@client.command(name = join)
+@client.command(name = 'hello', help = 'Поприветствовать бота')
+async def hello(ctx):
+	responses = ['Привет, создатель!', 'Добро пожаловать', 'こんにちは～～', 'こんばんは、みんなさん', 'おかえりなさいご主人']
+	print("hello")
+	await message.channel.send(choice(responses))
+
+
+@client.command(name = 'ping', help = 'Пропинговать бота')
+async def ping(ctx):
+	print(f'**Pong!** Latency: {round(client.latency * 1000)}ms')
+	await message.channel.send(f'**Pong!** Latency: {round(client.latency * 1000)}ms')
+
+
+@client.command(name = 'join', help = 'Подключить бота к голосовому каналу')
 async def join(ctx):
 	if not ctx.message.author.voice:
-		await ctx.send("You are not connected to a voice channel")
+		await ctx.send("Вы не в войс канале")
 		return
 
 	else:
@@ -32,39 +40,23 @@ async def join(ctx):
 
 	await channel.connect()
 
-@client.command()
+@client.command(name = 'leave', help = 'Отключить бота из голосового канала')
 async def leave(ctx):
 	voice_client = ctx.message.guild.voice_client
 	await voice_client.disconnect()
 	
-@client.command()
-async def foo(ctx, arg1,arg2):
-	print("foo")
-	await ctx.send(arg1+arg2)
+@client.command(name = 'summ', help = 'Сложить два числа X Y')
+async def summ(ctx, arg1, arg2):
+	await ctx.send(int(arg1)+int(arg2))
 
-@client.command()
-async def help(ctx):
-	await 
-
-
-@client.command()
-async def hello(ctx):
-	responses = ['Привет, создатель!', 'Добро пожаловать', 'こんにちは～～', 'こんばんは、みんなさん', 'おかえりなさいご主人']
-	print("hello")
-	await message.channel.send(choice(responses))
-
-
-@client.command()
-async def ping(ctx):
-	print(f'**Pong!** Latency: {round(client.latency * 1000)}ms')
-	await message.channel.send(f'**Pong!** Latency: {round(client.latency * 1000)}ms')
 
 
 
 @client.event
 async def on_message( message):
 		
-	
+	if message.content == 'Бот - молодец':
+			await message.channel.send('Спасибо! ❤️')
 	await client.process_commands(message)
 
 
